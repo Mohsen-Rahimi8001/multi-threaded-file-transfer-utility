9f54e88f0056a770bfb67aee732d91a725bffc3d
houmanhaghi <rezahaghighatgou@gmail.com>
2023-05-25 02:20:49 +0330

    help.txt added

--- src/client/client.c
+++ src/client/client.c
@@ -285,6 +285,13 @@ int main(int argc, char* argv[])
     // command:
     // ./main 127.0.0.1 -r pat patt pattt -c 10
 
+    if (argc == 2 && (strcmp(argv[1],"-h") == 0 || strcmp(argv[1],"--help") ==0)){
+        system("cat ../help.txt");
+        // system("cat \"$(whereis multithreadedfiletransfer.out)../help.txt\"");
+
+        exit(0);
+    }
+
     if (argc < 3) {
         printf("[-]Not enough arguments.\n");
         return 1;
@@ -318,7 +325,7 @@ int main(int argc, char* argv[])
     printf("[+]Connected to Server.\n");
 
     bool hasChunks;
-    if (strcmp(argv[argc - 2],"-c") == 0)
+    if (strcmp(argv[argc - 2],"-c") == 0 || strcmp(argv[argc - 2],"--count") == 0)
         if (isNumeric(argv[argc - 1])) {
             hasChunks = true;
             chunks = atoi(argv[argc - 1]);
@@ -336,7 +343,7 @@ int main(int argc, char* argv[])
     
     int number_of_files = 0;
     if (!hasChunks) number_of_files += 9;
-    if (strcmp(argv[2],"-r") != 0) number_of_files++;
+    if (strcmp(argv[2],"-r") != 0 || strcmp(argv[2],"--recursive") != 0) number_of_files++;
     else number_of_files += hasChunks ? argc - 5 : argc - 3;
 
     send_file_count(sockfd, number_of_files);
@@ -383,7 +390,7 @@ int main(int argc, char* argv[])
     }
 
 
-    if (strcmp(argv[2],"-r") != 0)
+    if (strcmp(argv[2],"-r") != 0 || strcmp(argv[2],"--recursive") != 0)
     {
         printf("> Partition 2\n");
         source_path = argv[2];        
@@ -408,7 +415,7 @@ int main(int argc, char* argv[])
     }
 
 
-    else if (strcmp(argv[2],"-r") == 0)
+    else if (strcmp(argv[2],"-r") == 0 || strcmp(argv[2],"--recursive") == 0)
     {
         printf("> Partition 3\n");
 
